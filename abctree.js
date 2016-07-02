3d8c02c98146b62df73aa6e07b0809009fd3b608
Chris Schalick <cschalick@yahoo.com>
2016-07-02 12:16:47 -0400

    add select + selected functions, formatting

--- abctree.js
+++ abctree.js
@@ -1,27 +1,29 @@
-// License information: MIT License
-//
-// Copyright (c) Chris Schalick 2015 All Rights Reserved.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is furnished
-// to do so, subject to the following conditions:
-//
-//   The above copyright notice and this permission notice shall be included in all
-//   copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/*
+License information: MIT License
 
-// Tree structure code creates an object hierarchy on demand
-// inside an HTML table, allows click to expand / collapse
-// to view and traverse the hierarchy.
+Copyright (c) Chris Schalick 2015 All Rights Reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Tree structure code creates an object hierarchy on demand
+inside an HTML table, allows click to expand / collapse
+to view and traverse the hierarchy.
+*/
 
 var abctree_style = document.createElement('style');
 abctree_style.type = 'text/css';
@@ -35,15 +37,24 @@ abctree_style = document.createElement('style');
 abctree_style.type = 'text/css';
 abctree_style.innerHTML = '.abctree_collapsed { background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAJCAIAAABv85FHAAAAXElEQVQImXXNQQrAIAwEQP1cHuUpn/QVxksl0FjL9qBQpXXZw8IE4ngfx8yx4NvXkiEZPAUxiCHVycSQKzyFWJAr8myewtzFjgv94uefNmgb1vdi5w1tiGWMYbs8uiCoqOdNhIkAAAAASUVORK5CYII="); width: 9px; height: 9px; float: left; margin: 3px; }';
 document.getElementsByTagName('head')[0].appendChild(abctree_style);
+abctree_style = document.createElement('style');
+abctree_style.type = 'text/css';
+abctree_style.innerHTML = '.abctree_selected { background-color: #00ff00; cursor: pointer }';
+document.getElementsByTagName('head')[0].appendChild(abctree_style);
+abctree_style = document.createElement('style');
+abctree_style.type = 'text/css';
+abctree_style.innerHTML = '.abctree_deselected { background-color: #ffffff; cursor: pointer }';
+document.getElementsByTagName('head')[0].appendChild(abctree_style);
 
 // abctree = root of the tree = table
 var abctree = function(id, parent_id){
-  var root = this;
+  "use strict";
   this.id = id;
   this.nodes = [];
   this.indent = 0;
   this.elem = document.createElement("table");
   this.elem.id = id;
+  this.selected = null;
 
   // insert the html element into the DOM
   document.getElementById(parent_id).appendChild(this.elem);
@@ -53,13 +64,19 @@ var abctree = function(id, parent_id){
 }
 
 abctree.prototype.node = function(id, val){
-  var kid = new abctree_node(this.root, this.elem, id, val);
+  "use strict";
+  var kid = new abctree_node(this, this.elem, id, val);
+  kid.tree = this;
   this.nodes.push(kid);
+  kid.select();
+  var callback = kid.make_callback(kid.select, kid);
+  kid.elem.children[1].onclick = callback;
   return kid;
 };
 
 // get all nodes in the tree
 abctree.prototype.all_below = function(){
+  "use strict";
   var below = [];
   for(var x = 0; x < this.nodes.length; x++)
     below.push(this.nodes[x]);
@@ -71,6 +88,7 @@ abctree.prototype.all_below = function(){
 
 // abctree_node = nodes in the tree = entries
 var abctree_node = function(parent, table, id, val){
+  "use strict";
   this.id = id;
   this.nodes = [];
   this.parent = parent;
@@ -78,7 +96,7 @@ var abctree_node = function(parent, table, id, val){
   this.elem = document.createElement("tr");
   this.elem.style.display = "block";
   this.elem.id = id;
-  this.elem.innerHTML = "<div id=" + id + ".img></div>" + val;
+  this.elem.innerHTML = "<div id=" + id + ".img></div><td id=" + id + ".val>" + val + "</td>";
   this.elem.children[0].className = "abctree_leaf";
   this.collapsed = true;
   this.table = table;
@@ -89,18 +107,41 @@ var abctree_node = function(parent, table, id, val){
   
 // make node invisible on page
 abctree_node.prototype.hide = function(){
+  "use strict";
   this.elem.style.display = "none";
 };
   
 // make node visible on page
 abctree_node.prototype.show = function(){
+  "use strict";
   this.elem.style.display = "block";
+};
+  
+// user clicked on selection
+abctree_node.prototype.select = function(opt){
+  "use strict";
+  var me;
+  if(opt)
+    me = opt;
+  else
+    me = this;
+  if(me.tree.selected != null)
+    me.tree.selected.deselect();
+  me.tree.selected = me;
+  me.elem.children[1].className = "abctree_selected";
+};
+  
+// user clicked on selection
+abctree_node.prototype.deselect = function(){
+  "use strict";
+  this.elem.children[1].className = "abctree_deselected";
 };
   
 // get either all nodes below or all nodes in the
 // next level down plus all nodes in expanded nodes
 // beyond that
 abctree_node.prototype.all_below = function(filter){
+  "use strict";
   var below = [];
 
   for(var x = 0; x < this.nodes.length; x++){
@@ -120,6 +161,7 @@ abctree_node.prototype.all_below = function(filter){
   
 // user clicked, collapse the tree below this node
 abctree_node.prototype.collapse = function(item){
+  "use strict";
   if(item.elem.children[0].className != "abctree_leaf"){
     item.elem.children[0].className = "abctree_collapsed";
     item.collapsed = true;
@@ -130,6 +172,7 @@ abctree_node.prototype.collapse = function(item){
 
 // user clicked, expand the tree below this node
 abctree_node.prototype.expand = function(item){
+  "use strict";
   if(item.elem.children[0].className != "abctree_leaf"){
     item.elem.children[0].className = "abctree_expanded";
     item.collapsed = false;
@@ -141,6 +184,7 @@ abctree_node.prototype.expand = function(item){
 
 // user clicked, either expand or collapse based on state
 abctree_node.prototype.toggle_expanded = function(item){
+  "use strict";
   if(item.elem.children[0].className == "abctree_expanded")
     item.collapse(item);
   else if(item.elem.children[0].className == "abctree_collapsed")
@@ -149,26 +193,33 @@ abctree_node.prototype.toggle_expanded = function(item){
 
 // save the instance info for later clicks
 abctree_node.prototype.make_callback = function(call, arg){
+  "use strict";
   var ret = function(){return call(arg);};
   return ret;
 }
 
 // add a child node beneath this one
 abctree_node.prototype.add_child = function(id, val){
+  "use strict";
   var child = new abctree_node(this, this.table, id, val);
   child.indent = this.indent + 10;
   child.elem.style.margin = "0px 0px 0px " + child.indent + "px";
   child.elem.style.display = "none";
   child.parent = this;
+  child.tree = this.tree;
   this.elem.children[0].className = "abctree_collapsed";
   var callback = this.make_callback(this.toggle_expanded, this);
   this.elem.children[0].onclick = callback;
+  callback = child.make_callback(child.select, child);
+  child.elem.children[1].onclick = callback;
   this.nodes.push(child);
+  child.select();
   return child;
 }
   
 // search function
 abctree_node.prototype.get_child_by_id = function(id){
+  "use strict";
   for(var x = 0; x < nodes.length; x++)
     if(nodes[x].id == id)
       return nodes[x];
